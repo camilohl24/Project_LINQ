63266ec1c6b2da4271d945e4e35f8af8007cb057
camilohl24 <juancamilohernandezhl@gmail.com>
2026-03-02 16:21:17 -0500

    Learning linq

---
+++ Project_LINQ/Fronted/Program.cs
@@ -0,0 +1,12 @@
+﻿List<string> names = new List<string>
+{
+    "JULIAN", "Ana", "Andrés", "María", "Alberto", "Sofía", "juaquin","FabiaN","Esteban"
+};
+List<string> filteredNames = names
+    .Where(L => L.EndsWith("n", StringComparison.InvariantCultureIgnoreCase))
+    .OrderBy(L => L)
+    .ToList();
+foreach (string name in filteredNames)
+{
+    Console.WriteLine(name);
+}
