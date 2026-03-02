4476afce8f76a8b9fb9fcf026931f3d30100350c
camilohl24 <juancamilohernandezhl@gmail.com>
2026-03-02 17:23:13 -0500

    Added teh product class and learned to work with LINQ

---
+++ Project_LINQ/Backend/Product.cs
@@ -0,0 +1,50 @@
+﻿namespace Backend
+{
+    public class Product
+    {
+        private string _name;
+        private double _price;
+
+        public Product(string name, double price)
+        {
+
+            Name = name;
+            Price = price;
+        }
+
+        public string Name
+        {
+           get => _name;
+            set => _name = ValidateName(value);
+
+        }
+        public double Price  
+        {
+
+            get => _price;
+            set => _price = ValidatePrice(value);
+        }
+
+
+        private double ValidatePrice(double price)
+        {
+            if (price < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(price),"El precio no puede ser negativo.");
+            }
+            return price;
+        }
+
+        private string ValidateName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentOutOfRangeException(nameof(name),"El nombre no puede estar vacío.");
+            }
+            return name;
+        }
+
+        public override string ToString() => $"Producto: {Name}\tPrecio: {Price,15:C}";
+        
+    }
+}
--- Project_LINQ/Fronted/Program.cs
+++ Project_LINQ/Fronted/Program.cs
@@ -1,4 +1,6 @@
-﻿List<string> names = new List<string>
+﻿using Backend;
+
+List<string> names = new List<string>
 {
     "JULIAN", "Ana", "Andrés", "María", "Alberto", "Sofía", "juaquin","FabiaN","Esteban"
 };
@@ -10,3 +12,21 @@ foreach (string name in filteredNames)
 {
     Console.WriteLine(name);
 }
+
+List<Product> productos = new List<Product>
+{
+    new Product (name : "Zapatos",  price : 50.0 ),
+    new Product (name : "Camisa",   price : 30.0),
+    new Product (name : "Pantalón", price : 40.0)
+};
+
+List<Product> filteredProducts = productos
+    .Where(p => p.Price > 35.0)
+    .Select(p => new Product(p.Name, p.Price * 1.20)) 
+    .ToList();
+
+foreach (Product producto in filteredProducts)
+{
+    Console.WriteLine(producto);  
+}
+
